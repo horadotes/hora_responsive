20bfa20288f093e5c9b4affdc98c4cb19b5e24dc
hzkhora <kaplogheinzschai@gmail.com>
2024-03-28 21:07:35 +0800

    final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./main/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#EDFCF2",
+        bgbasewhite: "#D0D5DD",
+        arrow: "#16B364",
+        btngreen: "#099250",
+        textgray: "#667085",
+      }
+    },
   },
   plugins: [],
 }
